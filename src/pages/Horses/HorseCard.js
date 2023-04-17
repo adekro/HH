0bd5d1e375c28b3fc4d74c7d9f6ad82bd624f43d
Adekro <e.croce88@gmail.com>
2023-04-17 17:41:25 +0200

    add horse page

--- src/pages/Horses/HorseCard.js
+++ src/pages/Horses/HorseCard.js
@@ -1,16 +1,32 @@
+import React, { useState } from "react";
 import classes from "./Horses.module.css";
 const HorseCard = ({ name, nickname, bread, gender, id }) => {
+	const [isChanged, setChenged] = useState(false);
+	const [valueName, setValueName] = useState(name);
+	const [valueNickName, setValueNickName] = useState(nickname);
+	const [valueBread, setValueBread] = useState(bread);
+	const [valueGender, setValueGender] = useState(gender);
+
+	const inputChangeHandler = () => {
+		setChenged(true);
+	};
+
 	return (
 		<div className={classes.content}>
 			<div className={classes.card}>
 				<img src={"/public/horse/" + id + ".png"} alt="" />
 			</div>
 			<div className={classes.info}>
-				<label>{name}</label>
-				<label>{nickname}</label>
-				<label>{bread}</label>
-				<label>{gender}</label>
+				<input type="text" value={valueName} onChange={inputChangeHandler} />
+				<input
+					type="text"
+					value={valueNickName}
+					onChange={inputChangeHandler}
+				/>
+				<input type="text" value={valueBread} onChange={inputChangeHandler} />
+				<input type="text" value={valueGender} onChange={inputChangeHandler} />
 			</div>
+			{isChanged && <button>Save</button>}
 		</div>
 	);
 };
