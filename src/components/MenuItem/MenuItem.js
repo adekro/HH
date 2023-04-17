78c91932eb75b34dbd0e1ab8143c303bdd33b19d
Adekro <e.croce88@gmail.com>
2023-04-17 17:28:21 +0200

    add horse page

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,18 +1,18 @@
 import React from "react";
 import classes from "./style.module.css";
 
-const MenuItem = (props) => {
+const MenuItem = ({ action, image, label, goPage }) => {
 	const handlerClick = () => {
-		props.goPage(props.action);
+		goPage(action);
 	};
 	return (
 		<React.Fragment>
 			<div className={classes.content} onClick={handlerClick}>
 				<div className={classes.image}>
-					<img src={props.image} alt="" />
+					<img src={image} alt="" />
 				</div>
 				<div className={classes.label}>
-					<label>{props.label}</label>
+					<label>{label}</label>
 				</div>
 			</div>
 		</React.Fragment>
