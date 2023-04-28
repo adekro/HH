d7ea6c11c31d41e692d630d9740991036951efda
adekro <33175405+adekro@users.noreply.github.com>
2023-04-28 22:06:20 +0200

    side

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Layout from './components/Layout/Layout';
+import React from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
 
-const App = () =>{
-
-  return(<React.Fragment>
-      <Layout>
-      </Layout>
-  </React.Fragment>);
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <Layout />
+    </React.Fragment>
+  );
+};
 
 export default App;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,44 +6,47 @@ import HorsesList from "../../pages/Horses/HorsesList";
 import useHorses from "../../hooks/useHorses";
 
 const Layout = (props) => {
-	const [page, setPage] = useState("home");
-	const { horses } = useHorses();
+  const [page, setPage] = useState("home");
+  const { horses } = useHorses();
 
-	const handlerGoPage = (action) => {
-		setPage(action);
-	};
+  const handlerGoPage = (action) => {
+    setPage(action);
+  };
 
-	return (
-		<React.Fragment>
-			<header>header</header>
-			<div className={classes.layoutbody}>
-				<div id="sidebar" className={classes.sidebar}>
-					<MenuItem
-						label="Home"
-						image=""
-						action="home"
-						goPage={handlerGoPage}
-					/>
-					<MenuItem
-						label="Cavalli"
-						image=""
-						action="horses"
-						goPage={handlerGoPage}
-					/>
-					<MenuItem
-						label="Farmaci"
-						image=""
-						action="farmaci"
-						goPage={handlerGoPage}
-					/>
-				</div>
-				<div id="content" className={classes.content}>
-					{page === "home" && <Dashboard load={page} />}
-					{page === "horses" && <HorsesList horses={horses} />}
-				</div>
-			</div>
-			<footer>footer</footer>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <header>header</header>
+      <div className={classes.layoutbody}>
+        <div id="sidebar" className={classes.sidebar}>
+          <div className={classes.sidebarcontent}>
+            <div className={classes.sidebarlist}>
+              <MenuItem
+                label="Home"
+                image=""
+                action="home"
+                goPage={handlerGoPage}
+              />
+              <MenuItem
+                label="Cavalli"
+                image=""
+                action="horses"
+                goPage={handlerGoPage}
+              />
+              <MenuItem
+                label="Farmaci"
+                image=""
+                action="farmaci"
+                goPage={handlerGoPage}
+              />
+            </div>
+          </div>
+        </div>
+        <div id="content" className={classes.content}>
+          {page === "home" && <Dashboard load={page} />}
+          {page === "horses" && <HorsesList horses={horses} />}
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 export default Layout;
